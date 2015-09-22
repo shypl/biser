d17e2b6e83beb41b1712f2ff6f8fef6fc86f62ba
shnyaka <shnyaka@gmail.com>
2015-09-22 16:33:18 +0300

    Develop

--- java/src/compiler/org/shypl/biser/compiler/Main.java
+++ java/src/compiler/org/shypl/biser/compiler/Main.java
@@ -2,10 +2,8 @@ package org.shypl.biser.compiler;
 
 import java.nio.file.Paths;
 
-public class Main
-{
-	public static void main(final String[] args)
-	{
+public class Main {
+	public static void main(final String[] args) {
 		try {
 			Compiler.run(Paths.get(args.length == 0 ? "biser.yml" : args[0]));
 		}
